c2f088d7e9d27e9dd04f7e1cd21178b7ce51222e
Mohammad Hajeer <mohammadhajeer002@gmail.com>
2024-09-14 08:49:18 +0300

    files uploaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+import "@/app/ui/global.css"
+import { inter } from "./ui/fonts";
+
+
+export const metadata:Metadata = {
+  title: "Acme",
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
